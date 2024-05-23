5be4255b2980c5ca1464aedbba9ee1b5bd1e86b5
bashartech <144168218+bashartech@users.noreply.github.com>
2024-05-24 01:51:36 +0500

    Add files via upload

---
+++ aap.ts
@@ -0,0 +1,38 @@
+// Question 25
+
+let align_color = "green"
+if(align_color === "green"){
+    console.log("Correct");
+    
+}else{
+    console.log("Incorrect!");
+}
+
+
+// Question 26
+
+let alien_colorr = "red"
+
+if(alien_colorr === "red"){
+    console.log("You Win");
+    
+}
+else{ 
+console.log("You lost");
+}
+
+// Question 27
+
+let alien_color = "blue"
+if(alien_color === "red"){
+    console.log("You Lost");
+    
+}else if(alien_color === "green"){
+    console.log("You lost");
+}else if(alien_color === "blue"){
+    console.log("You Won");
+}else{
+    console.log("You are out")
+}
+
+
